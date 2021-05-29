9bf1a71f4857600125f65f13c1debac9d842c2ac
Preetham14321 <79894297+Preetham14321@users.noreply.github.com>
2021-05-29 16:23:40 +0530

    feat : added new feature to web

--- js/script.js
+++ js/script.js
@@ -51,6 +51,18 @@ skillsHeader.forEach((el) => {
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
